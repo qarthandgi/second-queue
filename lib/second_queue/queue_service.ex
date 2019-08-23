85db259a7f6db5e94e85077596017d5a9da18a09
Niles Brandon <nbrandon360@gmail.com>
2019-08-23 09:03:10 -0500

    changes

--- lib/second_queue/queue_service.ex
+++ lib/second_queue/queue_service.ex
@@ -3,16 +3,9 @@ defmodule SecondQueue.QueueService do
   use Agent
 
   {:ok, agent_pid} = QueueStore.start_queues_link()
-  IO.puts(inspect {:ok, agent_pid})
-  IO.puts(Process.register(agent_pid, :qs))
-  IO.puts(inspect Process.whereis(:qs))
+  Process.register(agent_pid, :qs)
 
   def handle_incoming(queue, message) do
-    # QueueStore.add_queue(queue)
-    # content = Agent.get(:qs, fn content -> content end)
-    IO.puts("looking...")
-    IO.puts(Process.whereis(:qs))
-
     queue_atom = String.to_atom(queue)
     send(:qs, {:put, queue_atom, "success"})
   end
